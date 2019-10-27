cc197366c1157850653162f27f0a181ff7ef2b48
Hansanghyeon <dandan9509@gmail.com>
2019-10-28 08:58:27 +0900

    Study: () {} [] 짝맞추기 (best)

--- codewars/train/Valid-Braces.js
+++ codewars/train/Valid-Braces.js
@@ -6,13 +6,32 @@ function validBraces(braces){
   console.log("temp.indexOf('{}')", temp.indexOf('{}'));
   while(temp.indexOf('()') >= 0 || temp.indexOf('[]') >= 0 || temp.indexOf('{}') >= 0) {
     console.log('조건:', temp.indexOf('()') <= 0 && temp.indexOf('[]') <= 0 && temp.indexOf('{}') <= 0);
-    temp = temp.replace('()', '');
-    temp = temp.replace('[]', '');
-    temp = temp.replace('{}', '');
+    temp = temp.replace('()', '').replace('[]', '').replace('{}', '');
   }
   console.log('temp:', temp);
   console.log(temp.length);
   return temp.length <= 0;
+}
+
+// ----- best answer
+function best_validBraces(braces){
+  let matches = { '(':')', '{':'}', '[':']' };
+  let stack = [];
+  let currentChar;
+
+  for (let i=0; i<braces.length; i++) {
+    currentChar = braces[i];
+
+    if (matches[currentChar]) { // opening braces
+      stack.push(currentChar);
+    } else { // closing braces
+      if (currentChar !== matches[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; // any unclosed braces left?
 }
 
 //------- test mocha
