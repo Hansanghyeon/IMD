3ba9730a80765b68265b36b2083cb976df327245
Hansanghyeon <dandan9509@gmail.com>
2020-07-02 11:31:05 +0000

    study: listNode 만들기

--- coding-test/leetcode/2.add-two-numbers.js
+++ coding-test/leetcode/2.add-two-numbers.js
@@ -17,21 +17,49 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-  let result;
-  function createResult(first, second, listNext) {
-    console.log(first.val);
-    console.log(second.val);
-    if(first.next && second.next) {
-      listNext = new ListNode(first.val + second.val);
-    }
-    else {
-      console.log(result)
-      return result
-    };
-    createResult(first.next, second.next, listNext.next);
+
+const ListNodeToArray = (LN) => {
+  const instanceListNode = [];
+  let current = LN;
+  while (current !== null) {
+    instanceListNode.push(current.val);
+    current = current.next;
   }
-  console.log(createResult(l1, l2, result));
+  return instanceListNode;
+}
+var addTwoNumbers = function(l1, l2) {
+  let head = new ListNode(0);
+  let node = head;
+  let carry = 0;
+
+  while (l1 || l2) {
+    let l1Value = l1 ? l1.val : 0;
+    let l2Value = l2 ? l2.val : 0;
+    
+    let sum = l1Value + l2Value + carry;
+    carry = 0;
+    let newValue = sum;
+
+    if (sum > 9) {
+      newValue = sum % 10;
+      carry = 1;
+    }
+
+    node.next = new ListNode(newValue);
+    node = node.next;
+
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+
+  if (carry) {
+    node.next = new NodeList(carry);
+  }
+  return head.next;
 };
 // @lc code=end
 
