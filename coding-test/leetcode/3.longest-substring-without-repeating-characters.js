03c76dc15155376f270105f8aedd99ba2a4484e6
Hansanghyeon <dandan9509@gmail.com>
2020-07-08 18:22:08 +0000

    WIP: 겹치지 않는 최대길이의 문자열 찾기failed

--- coding-test/leetcode/3.longest-substring-without-repeating-characters.js
+++ coding-test/leetcode/3.longest-substring-without-repeating-characters.js
@@ -10,22 +10,24 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const result = [];
-  if (s.length === 1) return 1;
-  if (!s) return 0;
-  const result2 = s.split("").reduce((acc, cur) => {
-    if (!acc) return (acc = "");
-    if (acc.indexOf(cur) !== -1) {
-      result.push(acc);
-      acc = "";
+  let result = 0;
+  let compareString = "";
+
+  s.split("").forEach((e, index) => {
+    if (compareString.indexOf(e) === -1) {
+      compareString += e;
+      console.log(compareString);
+    } else if (compareString.length > result) {
+      result = compareString.length;
+      compareString = "";
     }
-    acc += cur;
-    return acc;
   });
-  result.sort((a, b) => b.length - a.length);
-  console.log(result, result2);
-  if (result.length === 0 || result[0].length < result2.length)
-    return result2.length;
-  return result[0].length;
+
+  if (compareString.length > result) {
+    result = compareString.length;
+    compareString = "";
+  }
+
+  return result;
 };
 // @lc code=end
