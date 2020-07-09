7ae04ec605ff0a23a3c10fd1912d7fe1749c71ec
Hansanghyeon <dandan9509@gmail.com>
2020-07-09 16:32:08 +0000

    study: go for only logic

--- go/workspaceSetting/src/for.go
+++ go/workspaceSetting/src/for.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
-	// for 문에 초기화 구문, 조건식, 후속 작업 정의
-	for i := 0; i < 10; i++ {
+	sum, i := 0, 0
+	// for 문에 조건식만 사용
+	for i < 10 {
 		sum += i
+		i++
 	}
 	fmt.Println(sum)
 }
