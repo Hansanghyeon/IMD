e0aaca28d5a31cf9d3dbef3d86a479a0061bac17
Hansanghyeon <dandan9509@gmail.com>
2020-08-09 15:08:36 +0900

    study: async await 리액트에서 사용하면서 실습

--- codespitz/88/client/src/App.js
+++ codespitz/88/client/src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const api2 = async (url, timeout = 50000, info = {}) => {
+  let id = -1;
+  const v = await Promise.race([
+    new Promise((res) => (id = window.setTimeout((_) => res(), timeout))),
+    fetch(new Request(url, info)),
+  ]);
+  if (v instanceof Response) {
+    clearTimeout(id);
+    if (v.status === 404) throw new Error("404");
+    return await v.json();
+  } else throw new Error("timeout");
+};
+
+const infinity = async function* (cat) {
+  let page = -1;
+  do {
+    try {
+      const { nextPage, items } = await api2(
+        `http://localhost:8080/list/${cat}/${page === -1 ? "" : page}`
+      );
+      page = nextPage;
+      yield items;
+    } catch (error) {
+      return;
+    }
+  } while (page !== -1);
+};
+const notice = infinity("notice");
+(async () => {
+  const { value, done } = await notice.next();
+  if (!done) console.log(value);
+})();
 
 function App() {
+  const hendleClick = async () => {
+    const { value, done } = await notice.next();
+    if (!done) console.log(value);
+    else console.log(value, done);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +56,8 @@ function App() {
         >
           Learn React
         </a>
+        <ul></ul>
+        <button onClick={hendleClick}>lead more</button>
       </header>
     </div>
   );
--- codespitz/88/server/route.js
+++ codespitz/88/server/route.js
@@ -2,13 +2,30 @@ const express = require("express");
 // const path = require('path');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.all("/*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+router.get("/", (req, res, next) => {
   res.write("Hello World!..."); //write a response to the client
   res.end(); //end the response
 });
-router.get("/list/notice/:id", (req, res) => {
-  res.write("notice page");
-  res.end();
+router.get("/list/notice", (req, res, next) => {
+  const result = {
+    nextPage: 1,
+    items: { id: 0 },
+  };
+  res.json(result);
+});
+router.get("/list/notice/:id", (req, res, next) => {
+  const id = req.params.id;
+  const result = {
+    nextPage: id === 100 ? -1 : new Number(id) + 1,
+    items: { id: new Number(id) },
+  };
+
+  res.json(result);
 });
 
 module.exports = router;
