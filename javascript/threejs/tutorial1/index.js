7788178a9d37890bff0c801676d71d0892490324
Hansanghyeon <dandan9509@gmail.com>
2020-08-20 18:16:45 +0900

    study: tutorial 3 - three.js

--- javascript/threejs/tutorial1/index.js
+++ javascript/threejs/tutorial1/index.js
@@ -8,8 +8,15 @@ var camera = new THREE.PerspectiveCamera(
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", function () {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
 
 // create the shape
 var geometry = new THREE.BoxGeometry(1, 1, 1);
