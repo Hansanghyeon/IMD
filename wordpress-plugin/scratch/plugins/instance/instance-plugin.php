2a054806f755fe0e660da193414be5456e3fac63
Hansanghyeon <dandan9509@gmail.com>
2020-08-25 12:18:35 +0900

    feat: my first wp plugin

---
+++ wordpress-plugin/scratch/plugins/instance/instance-plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package InstancePlugin
+ */
+
+/**
+ * Plugin Name: Instance Plugin
+ * Plugin URI: https://4log.hapas.io
+ * Description: 이 플러그인은 플러그인 개발용 테스트 플러그인입니다.
+ * Version: 1.0.0
+ * Author: 한상현
+ * Author URI: https://4log.hapas.io
+ * License: GPLv2 or later
+ * Text Domain: instance-plugin
+ */
+
+if (!defined('ABSPATH')) exit;
